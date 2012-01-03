6e09d24b9ac7bfe536780d7e8ebb12db1f7277fc
James Harton <jamesotron@gmail.com>
2012-01-04 08:13:11 +1300

    Simplify code, since we know we can only run on thin anyway. Also, wrap a thread around #listen so that it can be called from the router without blocking the application.

--- lib/faye-rails/rack_adapter.rb
+++ lib/faye-rails/rack_adapter.rb
@@ -3,29 +3,15 @@ require 'faye'
 module FayeRails
   class RackAdapter < ::Faye::RackAdapter
 
-    ERROR_WEBSOCKETS_DISABLED_RESPONSE = [400, { :content_type => 'text/plain', :content_length => 30 }, ["Error: WebSockets not enabled."]].freeze
-
     attr_reader :server, :endpoint
 
     def initialize(app=nil, options=nil)
       super
-      @enable_websockets = @options.delete(:enable_websockets)
       FayeRails.servers << self
     end
 
-    def call(env)
-      if @enable_websockets && env['HTTP_UPGRADE']
-        ws = ::Faye::WebSocket.new(env)
-        ws.onmessage = lambda do |event|
-          ws.send(event.data)
-        end
-        ws.onclose = lambda do |event|
-          ws = nil
-        end
-        ASYNC_RESPONSE
-      elsif env['HTTP_UPGRADE']
-        ERROR_WEBSOCKETS_DISABLED_RESPONSE
-      else
+    def listen(port, ssl_options = nil)
+      Thread.new do
         super
       end
     end
