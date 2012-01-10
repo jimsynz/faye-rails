0c7fb063ae59aa91a9ed4d0098af46ed03c62e6a
James Harton <jamesotron@gmail.com>
2012-01-11 11:23:51 +1300

    Stub out basic channel routing.

--- lib/faye-rails/rack_adapter.rb
+++ lib/faye-rails/rack_adapter.rb
@@ -19,5 +19,81 @@ module FayeRails
       end
     end
 
+    def routing_extension
+      if @routing_extension
+        @routing_extension
+      else
+        @routing_extension = RoutingExtension.new
+        server.add_extension(@routing_extension)
+      end
+    end
+
+    def map(opts)
+      routing_extension
+      if opts.is_a? Hash
+        opts.each do |channel, controller|
+          if channel.is_a? String
+            routing_extension.map(channel, controller)
+          elsif channel == :default
+            if controller == :block
+              routing_extension.block_unknown_channels!
+            elsif controller == :drop
+              routing_extension.drop_unknown_channels!
+            elsif controller == :allow
+              routing_extension.allow_unknown_channels!
+            end
+          end
+        end
+      end
+    end
+
+    class RoutingExtension < Filter
+
+      def initialize
+        @default = :allow
+        @mappings = {}
+        super nil, :any do
+          if message['channel'] == '/meta/subscribe'
+            take_action_for message['subscription']
+          elsif message['channel'] == '/meta/unsubscribe'
+            take_action_for message['subscription']
+          elsif message['channel'][0..5] == '/meta/'
+            pass
+          elsif message['channel'][0..8] == '/service/'
+            pass
+          else
+            take_action_for message['channel']
+          end
+        end
+      end
+
+      def block_unknown_channels!
+        @default = :block
+      end
+
+      def drop_unknown_channels!
+        @default = :drop
+      end
+
+      def allow_unknown_channels!
+        @default = :allow
+      end
+
+      def take_action_for(test)
+        if @mappings.keys.member? test
+          pass
+        elsif @default == :block
+          block "Permission denied."
+        elsif @default == :drop
+          drop
+        elsif @default == :allow
+          allow
+        else
+          drop
+        end
+      end
+
+    end
+
   end
 end
