58c79f57cac86bfed5db3c5d2acb8dbcc1809ffc
Ryan Lovelett <ryan@wahvee.com>
2012-06-24 10:00:57 -0500

    Change pec loading.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,8 @@ ENV['RAILS_ENV'] ||= 'test'
 # in spec/support/ and its subdirectories.
 Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each {|f| require f}
 Dir[File.join(File.dirname(__FILE__), "dummy/spec/**/*.rb")].each {|f| require f}
+require 'rspec/autorun'
+require 'faye-rails'
 require 'database_cleaner'
 require 'fiber'
 
@@ -15,14 +17,26 @@ end
 Rails.backtrace_cleaner.remove_silencers!
 
 RSpec.configure do |config|
+  # == Mock Framework
+  #
+  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+  #
+  # config.mock_with :mocha
+  # config.mock_with :flexmock
+  # config.mock_with :rr
   config.mock_with :mocha
+
+  # Include the Within Module
   config.include(Within)
+
+  # Configure transactions
   config.before(:suite) do
     DatabaseCleaner.strategy = :truncation
     DatabaseCleaner.start
   end
+
   config.after(:suite) do
     DatabaseCleaner.clean
   end
-end
 
+end
