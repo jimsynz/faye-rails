8c18264eb2c38e961efbd0bf63ff86f2ff925019
James Harton <james@resistor.io>
2014-03-11 15:03:56 +1300

    Fatal error when the user has the `Rack::Lock` in the stack.

--- lib/faye-rails/middleware.rb
+++ lib/faye-rails/middleware.rb
@@ -13,6 +13,26 @@ module FayeRails
     def initialize(app, options={}, &block)
       @app = app
 
+      if Rails.application.config.middleware.include? Rack::Lock
+        message = <<-EOF
+
+WARNING: You have the Rack::Lock middlware enabled.
+
+faye-rails can't work when Rack::Lock is enabled, as it will cause
+a deadlock on every request.
+
+Please add:
+
+    config.middleware.delete Rack::Lock
+
+to your application config in application.rb
+
+        EOF
+        Rails.logger.fatal message
+        $stdout.puts message
+        exit 1
+      end
+
       unknown_options = options.keys - DEFAULTS.keys
       if unknown_options.one?
         raise ArgumentError, "Unknown option: #{unknown_options.first}."
